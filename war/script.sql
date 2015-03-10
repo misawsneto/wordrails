24bd57fa0405140e15b9aeffc126912ecb08cce1
Misael Neto <misawsneto@gmail.com>
2015-03-10 18:36:15 -0300

    update sql script

--- war/script.sql
+++ war/script.sql
@@ -17,5 +17,6 @@ insert into network_taxonomy (networks_id, taxonomies_id) values (1, 1);
 -- insert into taxonomy (name, type) values ('Categoria', 'N');
 -- insert into network (name, subdomain, defaultTaxonomy_id) values ('portodigital', 'portodigital', 34);
 -- insert into person_network_role (admin, network_id, person_id) values (1, 19, 82);
+-- insert into person_network_role (admin, network_id, person_id) values (0, 19, 1);
 -- update taxonomy set owningNetwork_id = 19 where id = 34;
 -- insert into network_taxonomy (networks_id, taxonomies_id) values (19, 34);
