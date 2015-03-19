c27224c9f257afa0a5f8bf4d7f22dbffb1ff481b
Misael Neto <misawsneto@gmail.com>
2015-03-19 12:42:56 -0300

    updates

--- war/src/main/java/com/wordrails/api/CORSCustomFilter.java
+++ war/src/main/java/com/wordrails/api/CORSCustomFilter.java
@@ -27,12 +27,11 @@ public class CORSCustomFilter implements Filter{
 		HttpServletRequest reqt = (HttpServletRequest) req;
 		String method = reqt.getMethod().toLowerCase();
 		
-		if(method.equals("get") || method.equals("post") || method.equals("put") || method.equals("head") || method.equals("options"))
-			setCorsHeader(reqt, resp);
 		if(method.equals("head") || method.equals("options")){
 		    resp.setStatus(HttpServletResponse.SC_OK);
 		    return;
 		}
+		setCorsHeader(reqt, resp);
 		
 		chain.doFilter(req, res);
 	}
@@ -41,13 +40,13 @@ public class CORSCustomFilter implements Filter{
 		String origin = reqt.getHeader("Origin");
 		if(origin == null || origin.isEmpty())
 			origin = "*";
-		
+
 		resp.setHeader("Access-Control-Allow-Origin", origin);
 		resp.setHeader("Access-Control-Expose-Headers", "Access-Control-Allow-Origin,Access-Control-Allow-Credentials");
 		resp.setHeader("Access-Control-Allow-Credentials", "true");
+		resp.setHeader("Access-Control-Max-Age", "3600");
 		resp.setHeader("Access-Control-Allow-Headers", "Authorization,Content-Type,Content-Range,Content-Disposition,Content-Description,X-Requested-With,accept,Origin,Access-Control-Request-Method,Access-Control-Request-Headers");
-		resp.setHeader("Access-Control-Allow-Methods", "GET,POST,HEAD,OPTIONS,PUT");
-		resp.setHeader("P3P", "CP=\"CAO PSA CONi OTR OUR DEM ONL\"");
+		resp.setHeader("Access-Control-Allow-Methods", "GET,POST,DELETE,PUT,HEAD,OPTIONS");
 	}
 
 	@Override
