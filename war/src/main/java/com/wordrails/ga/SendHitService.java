54d39750533d9f0390b6ea89d26df60269826e6a
Misael Neto <misawsneto@gmail.com>
2015-03-27 17:51:49 -0300

    correções.

--- war/src/main/java/com/wordrails/ga/SendHitService.java
+++ war/src/main/java/com/wordrails/ga/SendHitService.java
@@ -7,16 +7,16 @@ import com.github.tasubo.jgmp.Decorating;
 import com.github.tasubo.jgmp.MpClient;
 
 class SendHitService {
-	@ServiceActivator
-	public void sendHit(final Hit hit) {
-		Decorating userIp = new Decorating() {			
-			@Override
-			public String getPart() {
-				return "&uip=" + hit.userIp;
-			}
-		};
-		ClientID client = ClientID.seeded(hit.clientSeed);
-		MpClient analytics = MpClient.withTrackingId(hit.trackingId).create();
-		analytics.send(hit.sendable.with(client).with(userIp));
-	}
+//	@ServiceActivator
+//	public void sendHit(final Hit hit) {
+//		Decorating userIp = new Decorating() {			
+//			@Override
+//			public String getPart() {
+//				return "&uip=" + hit.userIp;
+//			}
+//		};
+//		ClientID client = ClientID.seeded(hit.clientSeed);
+//		MpClient analytics = MpClient.withTrackingId(hit.trackingId).create();
+//		analytics.send(hit.sendable.with(client).with(userIp));
+//	}
 }
