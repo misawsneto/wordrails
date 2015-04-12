e444266c60fbf879ee5116958c786849cefcaf65
Misael Neto <misawsneto@gmail.com>
2015-04-12 12:32:30 -0300

    implementando segurança a favoritos

--- jar/src/main/java/com/wordrails/persistence/FavoriteRepository.java
+++ jar/src/main/java/com/wordrails/persistence/FavoriteRepository.java
@@ -1,10 +1,15 @@
 package com.wordrails.persistence;
 
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QueryDslPredicateExecutor;
+import org.springframework.data.repository.query.Param;
 
 import com.wordrails.business.Favorite;
 
 public interface FavoriteRepository extends JpaRepository<Favorite, Integer>, QueryDslPredicateExecutor<Favorite> {
 
+//	List<Favorite> findByPersonUsername(@Param("username") String username, Pageable pageable);
 }
