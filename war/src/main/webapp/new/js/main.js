bae1fd0157f10ec0d2b23efa09e51be77d1abe88
Misael Neto <misawsneto@gmail.com>
2015-04-20 20:29:55 -0300

    web client updates

--- war/src/main/webapp/new/js/main.js
+++ war/src/main/webapp/new/js/main.js
@@ -98,6 +98,7 @@ angular.module('app')
       });
 
       $scope.backgroundImage = function(postView, size){
+        console.log(postView);
         var img = $filter('pvimageLink')(postView, size);
         return img;
       }
