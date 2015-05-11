118f4bfe30b48376a5bd7e5fac35742a32d38638
Misael Neto <misawsneto@gmail.com>
2015-05-11 15:40:35 -0300

    updates...

--- war/src/main/webapp/new/js/directives/custom.js
+++ war/src/main/webapp/new/js/directives/custom.js
@@ -78,84 +78,38 @@
   }// end of return 
 })
 
- .directive('offClick', ['$rootScope', '$parse', function ($rootScope, $parse) {
-  var id = 0;
-  var listeners = {};
+ .directive('clickOutside', ['$document',function ($document) {
 
-  document.addEventListener("touchend", offClickEventHandler, true);
-  document.addEventListener('click', offClickEventHandler, true);
-
-  function targetInFilter(target, elms) {
-    if (!target || !elms) return false;
-    var elmsLen = elms.length;
-    for (var i = 0; i < elmsLen; ++i)
-      if (elms[i].contains(target)) return true;
-    return false;
-  }
-
-  function offClickEventHandler(event) {
-    var target = event.target || event.srcElement;
-    angular.forEach(listeners, function (listener, i) {
-      if (!(listener.elm.contains(target) || targetInFilter(target, listener.offClickFilter))) {
-        $rootScope.$evalAsync(function () {
-          listener.cb(listener.scope, {
-            $event: event
-          });
-        })
-      }
-
-    });
-  }
-
-  return {
+   return {
     restrict: 'A',
-    compile: function ($element, attr) {
-      var fn = $parse(attr.offClick);
-      return function (scope, element) {
-        var elmId = id++;
-        var offClickFilter;
-        var removeWatcher;
-        
-        offClickFilter = document.querySelectorAll(scope.$eval(attr.offClickFilter));
+    scope: {
+      clickOutside: '&'
+    },
+    link: function ($scope, elem, attr) {
+      var classList = (attr.outsideIfNot !== undefined) ? attr.outsideIfNot.replace(', ', ',').split(',') : [];
+      if (attr.id !== undefined) classList.push(attr.id);
 
-        if (attr.offClickIf) {
-          removeWatcher = $rootScope.$watch(function () {
-            return $parse(attr.offClickIf)(scope);
-          }, function (newVal) {
-            if (newVal) {
-              on();
-            } else if (!newVal) {
-              off();
+      $document.on('click', function (e) {
+        var i = 0,
+        element;
+
+        if (!e.target) return;
+
+        for (element = e.target; element; element = element.parentNode) {
+          var id = element.id;
+          var classNames = element.className;
+
+          if (id !== undefined) {
+            for (i = 0; i < classList.length; i++) {
+              if (id.indexOf(classList[i]) > -1 || classNames.indexOf(classList[i]) > -1) {
+                return;
+              }
             }
-          });
-        } else {
-          on();
-        }
-
-        attr.$observe('offClickFilter', function (value) {
-          offClickFilter = document.querySelectorAll(scope.$eval(value));
-        });
-
-        scope.$on('$destroy', function () {
-          off();
-          if (removeWatcher) {
-            removeWatcher();
           }
-        });
-
-        function on() {
-          listeners[elmId] = {
-            elm: element[0],
-            cb: fn,
-            scope: scope,
-            offClickFilter: offClickFilter
-          };
         }
 
-        function off() {
-          delete listeners[elmId];
-        }
-      };
+        $scope.$eval($scope.clickOutside);
+      });
     }
   };
 }])
