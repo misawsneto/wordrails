f1b4bd7556ec2afaf96b390c23f1d61258bbfa10
Misael Neto <misawsneto@gmail.com>
2015-06-25 21:33:21 -0300

    Ordenação dos comentários.

--- jar/src/main/java/com/wordrails/persistence/CommentRepository.java
+++ jar/src/main/java/com/wordrails/persistence/CommentRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RestResource;
@@ -15,5 +16,6 @@ public interface CommentRepository extends JpaRepository<Comment, Integer>, Quer
 	@RestResource(exported=false)
 	List<Comment> findByPost(Post post);
 
+	@Query("SELECT comment FROM Comment comment WHERE comment.post.id = :postId ORDER BY comment.id DESC")
 	List<Comment> findPostCommentsOrderByDate(@Param("postId") Integer postId, Pageable pageable);
 }
