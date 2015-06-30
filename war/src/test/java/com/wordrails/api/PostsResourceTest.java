ba482819b7561e680533b35a4f7ba8e6f292a76d
Arthur Taborda <arthur.hvt@gmail.com>
2015-06-30 14:09:48 -0300

    fix test for strings

--- war/src/test/java/com/wordrails/api/PostsResourceTest.java
+++ war/src/test/java/com/wordrails/api/PostsResourceTest.java
@@ -31,11 +31,17 @@ public class PostsResourceTest extends AbstractTest {
 		FullTextEntityManager ftem = org.hibernate.search.jpa.Search.getFullTextEntityManager(manager);
 		QueryBuilder qb = ftem.getSearchFactory().buildQueryBuilder().forEntity(Post.class).get();
 
+		String q = "schwrchnegareaead";
 
-		org.apache.lucene.search.Query t1 = qb.keyword().fuzzy().onField("id").ignoreAnalyzer().matching(1633).createQuery();
-		List<Post> posts = ftem.createFullTextQuery(t1).getResultList();
+		//org.apache.lucene.search.Query t1 = qb.keyword().fuzzy().onField("id").ignoreAnalyzer().matching(1633).createQuery();
+		org.apache.lucene.search.Query t1 = qb.keyword().fuzzy().withThreshold(.8f).withPrefixLength(1).onField("title").
+				boostedTo(5).andField("body").boostedTo(2).andField("topper").andField("subheading").andField("author.name").
+				andField("terms.name").ignoreAnalyzer().matching(q).createQuery();
+
+		javax.persistence.Query persistenceQuery = ftem.createFullTextQuery(t1, Post.class);
+		List<Post> posts = persistenceQuery.getResultList();
 		for(Post p : posts) {
-			System.out.println(p.body);
+			System.out.println(p.title + " : " + p.body);
 		}
 		System.out.println(ftem.createFullTextQuery(t1).getResultSize());
 	}
