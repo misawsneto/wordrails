d4d3bca2c2ee975971eae043f6331dce06fe841b
Misael Neto <misawsneto@gmail.com>
2015-07-13 06:08:33 -0300

    fixes

--- war/script2.sql
+++ war/script2.sql
@@ -5,7 +5,7 @@ ALTER TABLE PersonNetworkRegId MODIFY person_id INT(11) default NULL;
 ALTER TABLE PersonNetworkToken MODIFY person_id INT(11) default NULL;
 ALTER TABLE PostRead MODIFY person_id INT(11) default NULL;
 
-ALTER TABLE Network defaultTaxonomy_id INT(11) default NULL;
+ALTER TABLE Network MODIFY defaultTaxonomy_id INT(11) default NULL;
 
 ALTER TABLE station drop column social;
 
