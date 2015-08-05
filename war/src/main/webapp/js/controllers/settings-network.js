ca7016bca4420c54e1bfa18b1627086990e06052
Misael Neto <misawsneto@gmail.com>
2015-08-05 12:07:00 -0300

    network image update

--- war/src/main/webapp/js/controllers/settings-network.js
+++ war/src/main/webapp/js/controllers/settings-network.js
@@ -1,175 +1,165 @@
-app.controller('SettingsNetworkCtrl', ['$scope', '$log', '$timeout', '$mdDialog', '$state', 'trix', 'FileUploader', 'TRIX',
-	function($scope ,  $log ,  $timeout ,  $mdDialog ,  $state, trix , FileUploader, TRIX){
+app.controller('SettingsNetworkCtrl', ['$scope', '$log', '$timeout', '$mdDialog', '$state', 'trix', 'FileUploader', 'TRIX', 'cfpLoadingBar', '$mdToast',
+	function($scope ,  $log ,  $timeout ,  $mdDialog ,  $state, trix , FileUploader, TRIX, cfpLoadingBar, $mdToast){
 		$scope.app.lastSettingState = "app.settings.network";
 		$scope.network = angular.copy($scope.app.initData.network)
 
-		FileUploader.FileSelect.prototype.isEmptyAfterSelection = function() {
+		$scope.loginImage = {
+			link: $scope.network.loginImageId ? TRIX.baseUrl + "/api/files/" + $scope.network.loginImageId + "/contents" : null}
+
+		$scope.splashImage = {
+			link: $scope.network.splashImageId ? TRIX.baseUrl + "/api/files/" + $scope.network.splashImageId + "/contents" : null}
+
+		$scope.faviconImage = {
+			link: $scope.network.faviconId ? TRIX.baseUrl + "/api/files/" + $scope.network.faviconId + "/contents" : null}
+
+
+	FileUploader.FileSelect.prototype.isEmptyAfterSelection = function() {
 	    return true; // true|false
-	  };
+	};
 
-	  $scope.baseUrl = TRIX.baseUrl;
+	var login = $scope.login = new FileUploader({
+		url: TRIX.baseUrl + "/api/files/contents/simple"
+	});
 
-	  var loginUploader = $scope.loginUploader = new FileUploader({
-	  	url: TRIX.baseUrl + "/api/files/contents/simple"
-	  });
+	var splash = $scope.splash = new FileUploader({
+		url: TRIX.baseUrl + "/api/files/contents/simple"
+	});
 
-	  var splashUploader = $scope.splashUploader = new FileUploader({
-	  	url: TRIX.baseUrl + "/api/files/contents/simple"
-	  });
+	var favicon = $scope.favicon = new FileUploader({
+		url: TRIX.baseUrl + "/api/files/contents/simple"
+	});
 
-	  var faviconUploader = $scope.faviconUploader = new FileUploader({
-	  	url: TRIX.baseUrl + "/api/files/contents/simple"
-	  });
+	login.onAfterAddingFile = function(fileItem) {
+		$scope.loginImage = null;
+		login.uploadAll();
+	};
 
-	  loginUploader.onAfterAddingFile = function(fileItem) {
-	  	$scope.app.editingPost.uploadedImage = null;
-	  	loginUploader.uploadAll();
-	  };
+	login.onSuccessItem = function(fileItem, response, status, headers) {
+		if(response.filelink){
+			$scope.loginImage = response;
+			$mdToast.hide();
+		}
+	};
 
-	  loginUploader.onSuccessItem = function(fileItem, response, status, headers) {
-	  	if(response.filelink){
-	  		$scope.app.editingPost.uploadedImage = response;
-	  		$scope.app.editingPost.uploadedImage.filelink = TRIX.baseUrl + $scope.app.editingPost.uploadedImage.filelink
-	  		$scope.app.editingPost.showMediaButtons = false;
-	  		$scope.checkLandscape();
-	  		$("#image-config").removeClass("hide");
-	  		$mdToast.hide();
-	  		$scope.postCtrl.imageHasChanged = true
-	  	}
-	  };
+	login.onErrorItem = function(fileItem, response, status, headers) {
+		if(status == 413)
+			$scope.app.showErrorToast("A imagem não pode ser maior que 6MBs.");
+		else
+			$scope.app.showErrorToast("Não foi possível procesar a imagem. Por favor, tente mais tarde.");
+	}
 
-	  loginUploader.onErrorItem = function(fileItem, response, status, headers) {
-	  	if(status == 413)
-	  		$scope.app.showErrorToast("A imagem não pode ser maior que 6MBs.");
-	  	else
-	  		$scope.app.showErrorToast("Não foi possível procesar a imagem. Por favor, tente mais tarde.");
-	  }
+	login.onProgressItem = function(fileItem, progress) {
+		cfpLoadingBar.start();
+		cfpLoadingBar.set(progress/10)
+		if(progress == 100){
+			cfpLoadingBar.complete()
+			toastPromise = $mdToast.show(
+				$mdToast.simple()
+				.content('Processando...')
+				.position('top right')
+				.hideDelay(false)
+			);
+		}
+	};
 
-	  $scope.clearImage = function(){ 
-	  	$("#image-config").addClass("hide");
-	  	$scope.app.editingPost.uploadedImage = null;
-	  	loginUploader.clearQueue();
-	  	loginUploader.cancelAll()
-	  	$scope.checkLandscape();
-	  	$scope.postCtrl.imageHasChanged = true;
-	  }
+	splash.onAfterAddingFile = function(fileItem) {
+		$scope.splashImage = null;
+		splash.uploadAll();
+	};
 
-	  loginUploader.onProgressItem = function(fileItem, progress) {
-	  	cfpLoadingBar.start();
-	  	cfpLoadingBar.set(progress/10)
-	  	if(progress == 100){
-	  		cfpLoadingBar.complete()
-	  		toastPromise = $mdToast.show(
-	  			$mdToast.simple()
-	  			.content('Processando...')
-	  			.position('top right')
-	  			.hideDelay(false)
-	  			);
-	  	}
-	  };
+	splash.onSuccessItem = function(fileItem, response, status, headers) {
+		if(response.filelink){
+			$scope.splashImage = response;
+			$mdToast.hide();
+		}
+	};
 
-	  // -------------------- splash
+	splash.onErrorItem = function(fileItem, response, status, headers) {
+		if(status == 413)
+			$scope.app.showErrorToast("A imagem não pode ser maior que 6MBs.");
+		else
+			$scope.app.showErrorToast("Não foi possível procesar a imagem. Por favor, tente mais tarde.");
+	}
 
-	  splashUploader.onAfterAddingFile = function(fileItem) {
-	  	$scope.app.editingPost.uploadedImage = null;
-	  	splashUploader.uploadAll();
-	  };
+	splash.onProgressItem = function(fileItem, progress) {
+		cfpLoadingBar.start();
+		cfpLoadingBar.set(progress/10)
+		if(progress == 100){
+			cfpLoadingBar.complete()
+			toastPromise = $mdToast.show(
+				$mdToast.simple()
+				.content('Processando...')
+				.position('top right')
+				.hideDelay(false)
+				);
+		}
+	};
+	favicon.onAfterAddingFile = function(fileItem) {
+		$scope.faviconImage = null;
+		favicon.uploadAll();
+	};
 
-	  splashUploader.onSuccessItem = function(fileItem, response, status, headers) {
-	  	if(response.filelink){
-	  		$scope.app.editingPost.uploadedImage = response;
-	  		$scope.app.editingPost.uploadedImage.filelink = TRIX.baseUrl + $scope.app.editingPost.uploadedImage.filelink
-	  		$scope.app.editingPost.showMediaButtons = false;
-	  		$scope.checkLandscape();
-	  		$("#image-config").removeClass("hide");
-	  		$mdToast.hide();
-	  		$scope.postCtrl.imageHasChanged = true
-	  	}
-	  };
+	favicon.onSuccessItem = function(fileItem, response, status, headers) {
+		if(response.filelink){
+			$scope.faviconImage = response;
+			$mdToast.hide();
+		}
+	};
 
-	  splashUploader.onErrorItem = function(fileItem, response, status, headers) {
-	  	if(status == 413)
-	  		$scope.app.showErrorToast("A imagem não pode ser maior que 6MBs.");
-	  	else
-	  		$scope.app.showErrorToast("Não foi possível procesar a imagem. Por favor, tente mais tarde.");
-	  }
+	favicon.onErrorItem = function(fileItem, response, status, headers) {
+		if(status == 413)
+			$scope.app.showErrorToast("A imagem não pode ser maior que 6MBs.");
+		else
+			$scope.app.showErrorToast("Não foi possível procesar a imagem. Por favor, tente mais tarde.");
+	}
 
-	  $scope.clearImage = function(){ 
-	  	$("#image-config").addClass("hide");
-	  	$scope.app.editingPost.uploadedImage = null;
-	  	splashUploader.clearQueue();
-	  	splashUploader.cancelAll()
-	  	$scope.checkLandscape();
-	  	$scope.postCtrl.imageHasChanged = true;
-	  }
+	favicon.onProgressItem = function(fileItem, progress) {
+		cfpLoadingBar.start();
+		cfpLoadingBar.set(progress/10)
+		if(progress == 100){
+			cfpLoadingBar.complete()
+			toastPromise = $mdToast.show(
+				$mdToast.simple()
+				.content('Processando...')
+				.position('top right')
+				.hideDelay(false)
+				);
+		}
+	};
 
-	  splashUploader.onProgressItem = function(fileItem, progress) {
-	  	cfpLoadingBar.start();
-	  	cfpLoadingBar.set(progress/10)
-	  	if(progress == 100){
-	  		cfpLoadingBar.complete()
-	  		toastPromise = $mdToast.show(
-	  			$mdToast.simple()
-	  			.content('Processando...')
-	  			.position('top right')
-	  			.hideDelay(false)
-	  			);
-	  	}
-	  };
 
-	  // -------- login image
+	$scope.saveChanges = function(){
+		trix.putNetwork($scope.network).success(function(response){
+			$scope.app.getInitData();
+			$scope.app.showSuccessToast('Alterações realizadas com successo.')
+		})
 
-	  faviconUploader.onAfterAddingFile = function(fileItem) {
-	  	$scope.app.editingPost.uploadedImage = null;
-	  	faviconUploader.uploadAll();
-	  };
-	  
-	  faviconUploader.onSuccessItem = function(fileItem, response, status, headers) {
-	  	if(response.filelink){
-	  		$scope.app.editingPost.uploadedImage = response;
-	  		$scope.app.editingPost.uploadedImage.filelink = TRIX.baseUrl + $scope.app.editingPost.uploadedImage.filelink
-	  		$scope.app.editingPost.showMediaButtons = false;
-	  		$scope.checkLandscape();
-	  		$("#image-config").removeClass("hide");
-	  		$mdToast.hide();
-	  		$scope.postCtrl.imageHasChanged = true
-	  	}
-	  };
-
-	  faviconUploader.onErrorItem = function(fileItem, response, status, headers) {
-	  	if(status == 413)
-	  		$scope.app.showErrorToast("A imagem não pode ser maior que 6MBs.");
-	  	else
-	  		$scope.app.showErrorToast("Não foi possível procesar a imagem. Por favor, tente mais tarde.");
-	  }
-
-	  $scope.clearImage = function(){ 
-	  	$("#image-config").addClass("hide");
-	  	$scope.app.editingPost.uploadedImage = null;
-	  	faviconUploader.clearQueue();
-	  	faviconUploader.cancelAll()
-	  	$scope.checkLandscape();
-	  	$scope.postCtrl.imageHasChanged = true;
-	  }
-
-	  faviconUploader.onProgressItem = function(fileItem, progress) {
-	  	cfpLoadingBar.start();
-	  	cfpLoadingBar.set(progress/10)
-	  	if(progress == 100){
-	  		cfpLoadingBar.complete()
-	  		toastPromise = $mdToast.show(
-	  			$mdToast.simple()
-	  			.content('Processando...')
-	  			.position('top right')
-	  			.hideDelay(false)
-	  			);
-	  	}
-	  };
-
-	  $scope.saveChanges = function(){
-	  	trix.putNetwork($scope.network).success(function(response){
-	  		$scope.app.getInitData();
-	  		$scope.app.showSuccessToast('Alterações realizadas com successo.')
-	  	})
-	  }
-	}])
+		if($scope.loginImage && $scope.loginImage.id){
+			var loginImage = { original: TRIX.baseUrl + "/api/files/" + $scope.loginImage.id }
+			trix.postImage(loginImage).success(function(imageId){
+				var myLoginImage = TRIX.baseUrl + "/api/images/" + imageId;
+				$scope.network.loginImage = myLoginImage;
+				trix.putNetwork($scope.network).success(function(){
+				})
+			})
+		}
+		if($scope.splashImage && $scope.splashImage.id){
+			var splashImage = { original: TRIX.baseUrl + "/api/files/" + $scope.splashImage.id }
+			trix.postImage(splashImage).success(function(imageId){
+				var mySplashImage = TRIX.baseUrl + "/api/images/" + imageId;
+				$scope.network.splashImage = mySplashImage;
+				trix.putNetwork($scope.network).success(function(){
+				})
+			})
+		}
+		if($scope.faviconImage && $scope.faviconImage.id){
+			var favicon = { original: TRIX.baseUrl + "/api/files/" + $scope.faviconImage.id }
+			trix.postImage(favicon).success(function(imageId){
+				var myFavicon = TRIX.baseUrl + "/api/images/" + imageId;
+				$scope.network.favicon = myFavicon;
+				trix.putNetwork($scope.network).success(function(){
+				})
+			})
+		}
+	}
+}])
