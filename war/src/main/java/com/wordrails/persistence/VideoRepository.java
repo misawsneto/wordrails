9c3796cb800140d70be244d0988c24fd5f6b7a6b
Misael Neto <misawsneto@gmail.com>
2015-11-13 11:37:01 -0200

    refactoring 3

--- war/src/main/java/com/wordrails/persistence/VideoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.wordrails.persistence;
-
-import com.wordrails.business.Video;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.querydsl.QueryDslPredicateExecutor;
-
-public interface VideoRepository extends JpaRepository<Video, Integer>, QueryDslPredicateExecutor<Video> {
-}
