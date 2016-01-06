fe4ba944a6ca12ad528048e2243c041885d145b9
Arthur Taborda <arthur.hvt@gmail.com>
2016-01-06 18:05:11 -0300

    unique image scripts 100% in sql

--- war/src/main/resources/scripts/constraints.sql
+++ war/src/main/resources/scripts/constraints.sql
@@ -1,15 +1,4 @@
-ALTER TABLE post DROP FOREIGN KEY FK_eolhsgjjrm4w3rxg9q84vebfn;
-ALTER TABLE post DROP INDEX FK_eolhsgjjrm4w3rxg9q84vebfn;
-
-ALTER TABLE person DROP FOREIGN KEY FK_pfar63s78dw410jn5ub6b7105;
-ALTER TABLE person DROP INDEX FK_pfar63s78dw410jn5ub6b7105;
-ALTER TABLE person DROP FOREIGN KEY FK_rng9j4lteh8qu750nr38eko2r;
-ALTER TABLE person DROP INDEX FK_rng9j4lteh8qu750nr38eko2r;
-
-ALTER TABLE network DROP FOREIGN KEY FK_iqexgsjiwolldto8tc21lghxb;
-ALTER TABLE network DROP INDEX FK_iqexgsjiwolldto8tc21lghxb;
 ALTER TABLE network DROP FOREIGN KEY FK_nflo9f6ndf7e7s0ohaguxv13y;
-ALTER TABLE network DROP INDEX FK_nflo9f6ndf7e7s0ohaguxv13y;
-ALTER TABLE network DROP FOREIGN KEY FK_fm9gfvjplrafwmghykepqfgqv;
-ALTER TABLE network DROP INDEX FK_fm9gfvjplrafwmghykepqfgqv;
-ALTER TABLE network DROP FOREIGN KEY FK_hnqub15hlw0ijocvaix7s8oso;
+DROP INDEX UK_nflo9f6ndf7e7s0ohaguxv13y ON trix_dev.network;
+ALTER TABLE network ADD CONSTRAINT FK_nflo9f6ndf7e7s0ohaguxv13y FOREIGN KEY (favicon_id) REFERENCES image (id);
+
