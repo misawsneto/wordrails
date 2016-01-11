3d331038700961501b2e6bd4e99351f1f7a6efdf
Misael Neto <misawsneto@gmail.com>
2016-01-11 07:34:58 -0300

    perspective and main station update fixes

--- jar/src/main/java/co/xarx/trix/domain/StationPerspective.java
+++ jar/src/main/java/co/xarx/trix/domain/StationPerspective.java
@@ -4,11 +4,12 @@ package co.xarx.trix.domain;
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.io.Serializable;
 import java.util.Set;
 
 @Entity
 @Table(name="station_perspective")
-public class StationPerspective {
+public class StationPerspective implements Serializable{
 
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
