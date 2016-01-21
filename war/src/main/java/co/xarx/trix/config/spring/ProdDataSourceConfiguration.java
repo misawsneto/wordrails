22c3dd8ed86702e6b2a1135cf150a02670b623c5
Misael Neto <misawsneto@gmail.com>
2016-01-21 19:53:48 -0300

    notification test fixes in production

--- war/src/main/java/co/xarx/trix/config/spring/ProdDataSourceConfiguration.java
+++ war/src/main/java/co/xarx/trix/config/spring/ProdDataSourceConfiguration.java
@@ -1,5 +1,7 @@
 package co.xarx.trix.config.spring;
 
+import com.jolbox.bonecp.BoneCPDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -8,20 +10,26 @@ import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
+import javax.validation.Valid;
 
 @Configuration
 @Profile("prod")
 public class ProdDataSourceConfiguration {
 
-	@Bean
-	public DataSource dataSource() {
-		try {
-			Context ctx = new InitialContext();
-			return (DataSource) ctx.lookup("jdbc/trix");
-		} catch (NamingException e) {
-			e.printStackTrace();
-		}
+	@Value("#{systemProperties['username']}")
+	private String username;
 
-		return null;
+	@Value("#{systemProperties['password']}")
+	private String password;
+
+	@Bean
+	public BoneCPDataSource dataSource() {
+		BoneCPDataSource dataSource = new BoneCPDataSource();
+		dataSource.setDriverClass("com.mysql.jdbc.Driver");
+		dataSource.setJdbcUrl("jdbc:mysql://dbinstance1/wordrails4");
+		dataSource.setUsername(username);
+		dataSource.setPassword(password);
+
+		return dataSource;
 	}
 }
