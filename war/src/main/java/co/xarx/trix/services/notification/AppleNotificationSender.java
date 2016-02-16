fbdbb2fd500f14360cac5e55e49c29b6348bf3d5
jonasagx <jonas.agx@gmail.com>
2016-02-15 22:54:23 -0300

    Apple notification with new lib

--- war/src/main/java/co/xarx/trix/services/notification/AppleNotificationSender.java
+++ war/src/main/java/co/xarx/trix/services/notification/AppleNotificationSender.java
@@ -5,24 +5,18 @@ import co.xarx.trix.config.multitenancy.TenantContextHolder;
 import co.xarx.trix.domain.AppleCertificate;
 import co.xarx.trix.domain.Notification;
 import co.xarx.trix.persistence.AppleCertificateRepository;
-import co.xarx.trix.util.FileUtil;
+import com.notnoop.apns.APNS;
+import com.notnoop.apns.ApnsNotification;
+import com.notnoop.apns.ApnsService;
 import com.relayrides.pushy.apns.ApnsClient;
 import com.relayrides.pushy.apns.ClientNotConnectedException;
 import com.relayrides.pushy.apns.PushNotificationResponse;
-import com.relayrides.pushy.apns.util.ApnsPayloadBuilder;
 import com.relayrides.pushy.apns.util.SimpleApnsPushNotification;
 import com.relayrides.pushy.apns.util.TokenUtil;
-import io.netty.util.concurrent.Future;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.io.File;
 import java.io.IOException;
-import java.security.KeyManagementException;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.UnrecoverableKeyException;
-import java.security.cert.CertificateException;
 import java.sql.SQLException;
 import java.util.*;
 import java.util.concurrent.ExecutionException;
@@ -41,64 +35,75 @@ public class AppleNotificationSender implements NotificationSender {
 		this.appleCertificateRepository = appleCertificateRepository;
 	}
 
-	public ApnsClient<SimpleApnsPushNotification> getAPNsClient(Boolean isTest) throws SQLException,
-			CertificateException, UnrecoverableKeyException, NoSuchAlgorithmException, KeyStoreException,
-			KeyManagementException, IOException, InterruptedException {
+	public ApnsService getAPNsClient(Boolean isTest) throws SQLException {
+
 		String tenantId = TenantContextHolder.getCurrentTenantId();
 		AppleCertificate certificate = appleCertificateRepository.findByTenantId(tenantId);
 
-		File file = FileUtil.createNewTempFile(certificate.file.getBinaryStream());
+		ApnsService service = APNS
+				.newService()
+				.withCert(certificate.file.getBinaryStream(), certificate.password)
+				.withProductionDestination()
+				.build();
 
-		ApnsClient<SimpleApnsPushNotification> apnsClient = new ApnsClient<>(file, certificate.password);
-
-		Future<Void> connectFuture;
-		if (isTest) {
-			connectFuture = apnsClient.connect(ApnsClient.DEVELOPMENT_APNS_HOST);
-		} else {
-			connectFuture = apnsClient.connect(ApnsClient.PRODUCTION_APNS_HOST);
-		}
-		connectFuture.await();
-
-		return apnsClient;
+		return service;
 	}
 
 	@Override
 	public Map<String, NotificationResult> sendMessageToDevices(NotificationView notification, Collection<String> devices) throws IOException {
-		ApnsClient<SimpleApnsPushNotification> apnsClient;
+//		ApnsClient<SimpleApnsPushNotification> apnsClient;
+
+		ApnsService apnsClient = null;
 		try {
 			apnsClient = getAPNsClient(notification.test);
-		} catch (SQLException | CertificateException | UnrecoverableKeyException |
-				NoSuchAlgorithmException | KeyStoreException | KeyManagementException |InterruptedException e) {
-			throw new IOException("Failed to initialize Apple PushManager", e);
+		} catch (Exception e) {
+			throw new IOException("Failed to initialize Apple Notification Service");
 		}
 
-		ApnsPayloadBuilder payloadBuilder = new ApnsPayloadBuilder();
-		payloadBuilder.setBadgeNumber(1);
-		payloadBuilder.setSoundFileName("default");
-		payloadBuilder.addCustomProperty("postId", notification.postId);
-		payloadBuilder.setAlertTitle(notification.title);
-		payloadBuilder.setAlertBody(notification.message);
-		payloadBuilder.setContentAvailable(true);
+		String payload = APNS.newPayload()
+				.badge(1)
+				.sound("default")
+				.customField("postId", notification.postId)
+				.customField("title", notification.title)
+				.alertBody(notification.message)
+				.customField("content-available", true)
+				.shrinkBody("...")
+				.build();
 
-		return sendMessageToDevices(payloadBuilder.buildWithDefaultMaximumLength(), new ArrayList(devices), apnsClient, 0, new HashMap<>());
+		Collection<ApnsNotification> apnsNotifications = (Collection<ApnsNotification>) apnsClient.push(devices, payload);
+
+		Map<String, NotificationResult> notificationResultMap = new HashMap<>();
+		Map<String, Date> inactiveDevices = apnsClient.getInactiveDevices();
+
+		for (ApnsNotification apnsNotification : apnsNotifications) {
+			NotificationResult r = new NotificationResult();
+
+			if(inactiveDevices.containsKey(apnsNotification.getDeviceToken().toString())){
+				r.setStatus(Notification.Status.SEND_ERROR);
+				r.setErrorMessage("Device deactivated");
+				r.setDeviceDeactivated(true);
+			} else {
+				r.setStatus(Notification.Status.SUCCESS);
+			}
+
+			notificationResultMap.put(apnsNotification.getDeviceToken().toString(), r);
+		}
+
+		return notificationResultMap;
 	}
 
-	PushNotificationResponse get(ApnsClient<SimpleApnsPushNotification> apnsClient,
-										 SimpleApnsPushNotification apnsNotification) throws ExecutionException, InterruptedException {
+	PushNotificationResponse get(ApnsClient<SimpleApnsPushNotification> apnsClient, SimpleApnsPushNotification apnsNotification) throws ExecutionException, InterruptedException {
 		return apnsClient.sendNotification(apnsNotification).get();
 	}
 
-	private Map<String, NotificationResult> sendMessageToDevices(String payloader, List<String> devices,
-																ApnsClient<SimpleApnsPushNotification> apnsClient,
-																Integer index, Map<String, NotificationResult> results) throws IOException {
+	private Map<String, NotificationResult> sendMessageToDevices(String payloader, List<String> devices, ApnsClient<SimpleApnsPushNotification> apnsClient, Integer index, Map<String, NotificationResult> results) throws IOException {
 		int i = index;
 		try {
 			int notificationsCounter = 0;
 			for (i = index; i < devices.size(); i++) {
-				String deviceCode =  devices.get(i);
+				String deviceCode = devices.get(i);
 
-				SimpleApnsPushNotification apnsNotification =
-						new SimpleApnsPushNotification(TokenUtil.sanitizeTokenString(deviceCode), "topic", payloader);
+				SimpleApnsPushNotification apnsNotification = new SimpleApnsPushNotification(TokenUtil.sanitizeTokenString(deviceCode), "topic", payloader);
 
 				PushNotificationResponse response = get(apnsClient, apnsNotification);
 
