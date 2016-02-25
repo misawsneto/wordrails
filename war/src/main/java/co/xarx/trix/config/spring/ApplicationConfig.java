5688afd3022dd80e3c19acfeed855dbe0709a2d7
Arthur Taborda <arthur.hvt@gmail.com>
2016-02-25 19:08:14 -0300

    fixes on properties

--- war/src/main/java/co/xarx/trix/config/spring/ApplicationConfig.java
+++ war/src/main/java/co/xarx/trix/config/spring/ApplicationConfig.java
@@ -12,11 +12,17 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @Configuration
 @EnableAsync
+@PropertySource(name = "props",
+		value = {
+				"classpath:application-${spring.profiles.active:dev}.properties"
+		}
+)
 @ComponentScan(basePackages = "co.xarx.trix")
 public class ApplicationConfig {
 
