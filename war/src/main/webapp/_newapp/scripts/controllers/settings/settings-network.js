1a3b8f2347344c7438a164ad9252e36e29b3873c
Misael Neto <misawsneto@gmail.com>
2016-02-29 20:48:17 -0300

    refactoring css

--- war/src/main/webapp/_newapp/scripts/controllers/settings/settings-network.js
+++ war/src/main/webapp/_newapp/scripts/controllers/settings/settings-network.js
@@ -13,6 +13,37 @@ app.controller('SettingsNetworkCtrl', ['$scope', '$log', '$timeout', '$mdDialog'
 	      }
 	    }
 
+	    $scope.toggleLeft = buildDelayedToggler('left');
+	        /**
+     * Supplies a function that will continue to operate until the
+     * time is up.
+     */
+    function debounce(func, wait, context) {
+      var timer;
+      return function debounced() {
+        var context = $scope,
+            args = Array.prototype.slice.call(arguments);
+        $timeout.cancel(timer);
+        timer = $timeout(function() {
+          timer = undefined;
+          func.apply(context, args);
+        }, wait || 10);
+      };
+    }
+	    /**
+	     * Build handler to open/close a SideNav; when animation finishes
+	     * report completion in console
+	     */
+	    function buildDelayedToggler(navID) {
+	      return debounce(function() {
+	        $mdSidenav(navID)
+	          .toggle()
+	          .then(function () {
+	            $log.debug("toggle " + navID + " is done");
+	          });
+	      }, 200);
+	    }
+
 		$scope.app.lastSettingState = "app.settings.network";
 		$scope.network = angular.copy($scope.app.network)
 
