d367930d34a38f2876a17dafaae52683e8342ebd
jonasagx <jonas.agx@gmail.com>
2016-03-11 16:06:19 -0300

    Password update fix

--- war/src/main/java/co/xarx/trix/web/rest/PersonsResource.java
+++ war/src/main/java/co/xarx/trix/web/rest/PersonsResource.java
@@ -313,14 +313,14 @@ public class PersonsResource {
 	public void putPassword(@FormParam("oldPassword") String oldPassword, @FormParam("newPassword") String newPassword) {
 		Asserts.notEmpty(oldPassword, "Old password it empty or null");
 		Asserts.notEmpty(newPassword, "New password it empty or null");
-		Asserts.check((newPassword.length() < 5), "Password too short");
+
+		if(newPassword.length() < 5)throw new BadRequestException("Password too short");
 
 		Person loggedPerson = authProvider.getLoggedPerson();
 		if (!oldPassword.equals(loggedPerson.user.password)) throw new UnauthorizedException("Wrong password");
 
 		loggedPerson.user.password = newPassword;
 		personRepository.save(loggedPerson);
-
 	}
 
 	@POST
