0560b13f510d638de1277b8099ebfc92792197f1
Misael Neto <misawsneto@gmail.com>
2016-04-19 14:25:35 -0300

    load comments

--- war/src/main/webapp/scripts/controllers/app/page.js
+++ war/src/main/webapp/scripts/controllers/app/page.js
@@ -1,11 +1,24 @@
-app.controller('PageCtrl', ['$scope', '$log', '$timeout', '$mdDialog', '$state', 'TRIX', 'cfpLoadingBar', 'trixService', 'trix', '$http', '$mdToast', '$templateCache', '$location', '$interval', '$mdSidenav', '$translate', '$filter', '$localStorage', '$sce',
-	function($scope ,  $log ,  $timeout ,  $mdDialog ,  $state ,  TRIX ,  cfpLoadingBar ,  trixService ,  trix ,  $http ,  $mdToast, $templateCache  , $location, $interval, $mdSidenav, $translate, $filter, $localStorage, $sce){
+app.controller('PageCtrl', ['$scope', '$rootScope', '$log', '$timeout', '$mdDialog', '$state', 'TRIX', 'cfpLoadingBar', 'trixService', 'trix', '$http', '$mdToast', '$templateCache', '$location', '$interval', '$mdSidenav', '$translate', '$filter', '$localStorage', '$sce',
+	function($scope , $rootScope,  $log ,  $timeout ,  $mdDialog ,  $state ,  TRIX ,  cfpLoadingBar ,  trixService ,  trix ,  $http ,  $mdToast, $templateCache  , $location, $interval, $mdSidenav, $translate, $filter, $localStorage, $sce){
 
 		$timeout(function() {
 			$('.materialboxed').materialbox();
 		}, 2000);
 
 		pageCtrl = $scope;
+
+		$scope.reloadMasonry = function(){
+			$rootScope.$broadcast('masonry.reload');
+		}
+
+		$scope.loadComments = function(cell){
+			cell.loadingComments = true;
+			$timeout(function(){
+				cell.showComments = true; 
+				$scope.reloadMasonry()
+				cell.loadingComments = false;
+			},3000)
+		}
 
 }]);
 
