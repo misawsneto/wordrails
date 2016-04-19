cd0a89e324d04a9238851943e2ffa3a394876288
Misael Neto <misawsneto@gmail.com>
2016-04-19 08:29:37 -0300

    search improv 2

---
+++ war/src/main/webapp/scripts/controllers/app/search.js
@@ -0,0 +1,5 @@
+app.controller('SearchCtrl', ['$scope', '$log', '$timeout', '$mdDialog', '$state', 'TRIX', 'cfpLoadingBar', 'trixService', 'trix', '$http', '$mdToast', '$templateCache', '$location', '$interval', '$mdSidenav', '$translate', '$filter', '$localStorage',
+					function($scope ,  $log ,  $timeout ,  $mdDialog ,  $state ,  TRIX ,  cfpLoadingBar ,  trixService ,  trix ,  $http ,  $mdToast, $templateCache  , $location, $interval, $mdSidenav, $translate, $filter, $localStorage){
+	$scope.query = $state.params.q;
+	$scope.app.search.show=false;
+}]);
