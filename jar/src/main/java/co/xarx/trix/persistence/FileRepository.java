304c7ada9e21794a34a51b12ce039eb42ed29cb4
jonasagx <jonas.agx@gmail.com>
2016-04-28 17:07:11 -0300

    New method to calculate file's size

--- jar/src/main/java/co/xarx/trix/persistence/FileRepository.java
+++ jar/src/main/java/co/xarx/trix/persistence/FileRepository.java
@@ -5,10 +5,17 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RestResource;
 
+import java.util.List;
+
 public interface FileRepository extends DatabaseRepository<File, Integer> {
 
 	@RestResource(exported = false)
 	@Query("select hash from File where id=:id and type='E'")
 	String findExternalHashById(@Param("id") Integer id);
+
+	@RestResource(exported = false)
+	@Query("SELECT f.mime, SUM(f.size) FROM File f WHERE f.tenantId = :tenantId GROUP BY f.mime")
+	List<Object[]> sumFilesSizeByMime(@Param("tenantId") String tenantId);
+
 }
 
