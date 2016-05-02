2f2b0db1f5df5e974ad7342064695eb73a0c5b57
Misael Neto <misawsneto@gmail.com>
2016-05-02 12:16:35 -0300

    invitation emails

--- war/src/main/resources/db/migration/V15__invitation_fixes.sql
+++ war/src/main/resources/db/migration/V15__invitation_fixes.sql
@@ -1,4 +1,4 @@
 drop table invitation_station;
 drop table invitation;
 update network set invitationMessage = '{{inviterName}} convidou-lhe para participar da rede <strong class="uc"
-style="text-transform: uppercase;">{{networkName}}</strong>. Acesse o link e confira';
+style="text-transform: uppercase;">{{networkName}}</strong>.';
