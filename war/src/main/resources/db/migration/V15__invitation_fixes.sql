e66638a6f0dfa5726711f7b7cba9b4fb6cec951c
Misael Neto <misawsneto@gmail.com>
2016-05-02 11:25:52 -0300

    change network invitation message

--- war/src/main/resources/db/migration/V15__invitation_fixes.sql
+++ war/src/main/resources/db/migration/V15__invitation_fixes.sql
@@ -1,3 +1,4 @@
 drop table invitation_station;
 drop table invitation;
-update network set invitationMessage = ''
+update network set invitationMessage = '{{inviterName}} convidou-lhe para participar da rede <strong class="uc"
+style="text-transform: uppercase;">{{networkName}}</strong>. Acesse o link e confira';
