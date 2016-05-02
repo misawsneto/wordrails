aa9a98058646b20683218ace1f3b974793475331
jonasagx <jonas.agx@gmail.com>
2016-05-02 17:42:15 -0300

    Fix merge

--- war/src/main/java/co/xarx/trix/web/rest/api/v1/PersonsApi.java
+++ war/src/main/java/co/xarx/trix/web/rest/api/v1/PersonsApi.java
@@ -5,7 +5,6 @@ import co.xarx.trix.api.v2.StatsData;
 import co.xarx.trix.domain.Person;
 import co.xarx.trix.domain.StationRole;
 import co.xarx.trix.exception.ConflictException;
-import co.xarx.trix.util.StatsJson;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -173,7 +172,7 @@ public interface PersonsApi {
 	@GET
 	@Path("/me/stats")
 	@PreAuthorize("isAuthenticated()")
-	StatsJson personStats(@QueryParam("date") String date, @QueryParam("postId") Integer postId) throws IOException;
+	StatsData personStats(@QueryParam("date") String date, @QueryParam("postId") Integer postId) throws IOException;
 
 	@GET
 	@Path("/search/findPersons")
