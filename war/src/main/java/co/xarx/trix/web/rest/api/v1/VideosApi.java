90a94179247a3b682cf848d01a8b0efc2aa4bcaf
Misael Neto <misawsneto@gmail.com>
2016-05-05 18:09:26 -0300

    video api fix

--- war/src/main/java/co/xarx/trix/web/rest/api/v1/VideosApi.java
+++ war/src/main/java/co/xarx/trix/web/rest/api/v1/VideosApi.java
@@ -1,12 +1,17 @@
 package co.xarx.trix.web.rest.api.v1;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
 import javax.servlet.ServletException;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.io.IOException;
 
 @Path("/videos")
-@Consumes(MediaType.WILDCARD)
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
 public interface VideosApi {
 
 	@GET
