41a20984b63e5020ce7bfb01454467e4bd1c57cc
jonasagx <jonas.agx@gmail.com>
2016-05-06 16:29:54 -0300

    Emails validation tests

---
+++ war/src/test/java/co/xarx/trix/util/StringUtilTest.java
@@ -0,0 +1,24 @@
+package co.xarx.trix.util;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StringUtilTest {
+
+	@Before
+	public void setUp() throws Exception {
+
+	}
+
+	@Test
+	public void testToSlug() throws Exception {
+
+	}
+
+	@Test
+	public void testIsEmailAddr() throws Exception {
+
+	}
+}
