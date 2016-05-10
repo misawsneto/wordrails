8afe0b3717dcf605ba201260cbace3e9184dfd85
jonasagx <jonas.agx@gmail.com>
2016-05-09 21:40:33 -0300

    Methods to count perons with permission

--- war/src/main/java/co/xarx/trix/services/security/PersonPermissionService.java
+++ war/src/main/java/co/xarx/trix/services/security/PersonPermissionService.java
@@ -3,14 +3,19 @@ package co.xarx.trix.services.security;
 import co.xarx.trix.api.v2.PermissionData;
 import co.xarx.trix.api.v2.UserPermissionData;
 import co.xarx.trix.config.multitenancy.TenantContextHolder;
+import co.xarx.trix.config.security.Permissions;
+import co.xarx.trix.domain.Person;
+import co.xarx.trix.persistence.PersonRepository;
 import co.xarx.trix.persistence.StationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.acls.domain.PrincipalSid;
 import org.springframework.security.acls.model.AccessControlEntry;
 import org.springframework.security.acls.model.Acl;
 import org.springframework.security.acls.model.MutableAcl;
 import org.springframework.security.acls.model.ObjectIdentity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -19,12 +24,13 @@ public class PersonPermissionService {
 
 	private AccessControlListService aclService;
 	private StationRepository stationRepository;
+	private PersonRepository personRepository;
 
 	@Autowired
-	public PersonPermissionService(AccessControlListService aclService,
-								   StationRepository stationRepository) {
+	public PersonPermissionService(AccessControlListService aclService, StationRepository stationRepository, PersonRepository personRepository) {
 		this.aclService = aclService;
 		this.stationRepository = stationRepository;
+		this.personRepository = personRepository;
 	}
 
 	public UserPermissionData getPermissions(String username) {
@@ -55,5 +61,25 @@ public class PersonPermissionService {
 		result.getStationPermissions().add(permission);
 
 		return result;
+	}
+
+	public Iterable<Person> getPersonFromStation(Integer stationId) {
+		MutableAcl acl = aclService.findAcl(stationId);
+		List<Person> persons = new ArrayList<>();
+		for (AccessControlEntry entry : acl.getEntries()) {
+			boolean isNotUserPermission = !(entry.getSid() instanceof PrincipalSid);
+
+			if (isNotUserPermission) {
+				continue;
+			}
+
+			if (!entry.isGranting() && Permissions.containsPermission(entry.getPermission(), Permissions.READ)) {
+				continue;
+			}
+
+			PrincipalSid sid = (PrincipalSid) entry.getSid();
+			persons.add(personRepository.findByUsername(sid.getPrincipal()));
+		}
+		return persons;
 	}
 }
