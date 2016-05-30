1aa783e811e3f6093998014dfc0ca51ef24ac35d
jonasagx <jonas.agx@gmail.com>
2016-05-30 19:28:48 -0300

    Refactoring to log postId

--- war/src/main/java/co/xarx/trix/web/rest/resource/v2/V2BookmarksResource.java
+++ war/src/main/java/co/xarx/trix/web/rest/resource/v2/V2BookmarksResource.java
@@ -1,4 +1,36 @@
 package co.xarx.trix.web.rest.resource.v2;
 
-public class V2BookmarksResource {
+import co.xarx.trix.api.BooleanResponse;
+import co.xarx.trix.api.ContentResponse;
+import co.xarx.trix.api.PostView;
+import co.xarx.trix.services.post.PostService;
+import co.xarx.trix.web.rest.api.v2.V2BookmarksApi;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@NoArgsConstructor
+public class V2BookmarksResource implements V2BookmarksApi {
+	@Autowired
+	private PostService postService;
+
+	@Override
+	public ContentResponse<List<PostView>> searchBookmarks(String q, Integer page, Integer size) {
+
+		ContentResponse<List<PostView>> response = new ContentResponse<>();
+		response.content = postService.searchBookmarks(q, page, size);
+
+		return response;
+
+	}
+
+	@Override
+	public BooleanResponse toggleBookmark(Integer postId) {
+		BooleanResponse br = new BooleanResponse();
+		br.response = postService.toggleBookmark(postId);
+		return br;
+	}
 }
--- war/src/main/java/co/xarx/trix/web/rest/resource/v2/V2RecommendsResource.java
+++ war/src/main/java/co/xarx/trix/web/rest/resource/v2/V2RecommendsResource.java
@@ -1,4 +1,38 @@
 package co.xarx.trix.web.rest.resource.v2;
 
-public class V2RecommendsResource {
+import co.xarx.trix.api.BooleanResponse;
+import co.xarx.trix.api.ContentResponse;
+import co.xarx.trix.api.PostView;
+import co.xarx.trix.services.post.PostService;
+import co.xarx.trix.web.rest.api.v2.V2RecommendApi;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@NoArgsConstructor
+public class V2RecommendsResource implements V2RecommendApi {
+	private PostService postService;
+
+	@Autowired
+	public V2RecommendsResource(PostService postService) {
+		this.postService = postService;
+	}
+
+	@Override
+	public ContentResponse<List<PostView>> searchRecommends(String q, Integer page, Integer size) {
+		ContentResponse<List<PostView>> response = new ContentResponse<>();
+		response.content = postService.searchRecommends(q, page, size);
+
+		return response;
+	}
+
+	@Override
+	public BooleanResponse toggleRecommend(Integer postId) {
+		BooleanResponse br = new BooleanResponse();
+		br.response = postService.toggleRecommend(postId);
+		return br;
+	}
 }
