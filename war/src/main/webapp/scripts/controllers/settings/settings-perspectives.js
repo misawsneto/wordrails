2319872a4594bf808820fe0fb4018aceabbb3cd6
Misael Neto <misawsneto@gmail.com>
2016-06-10 18:55:44 -0300

    perspective sidebar

--- war/src/main/webapp/scripts/controllers/settings/settings-perspectives.js
+++ war/src/main/webapp/scripts/controllers/settings/settings-perspectives.js
@@ -1,6 +1,14 @@
-app.controller('SettingsPerspectivesCtrl', ['$scope', '$log', '$timeout', '$mdDialog', '$state', 'trix', 'FileUploader', 'TRIX', 'cfpLoadingBar', '$mdDialog', '$mdToast', '$filter', '$translate', '$mdConstant',
-	function($scope ,  $log ,  $timeout ,  $mdDialog ,  $state, trix, FileUploader, TRIX, cfpLoadingBar, $mdDialog, $mdToast, $filter, $translate, $mdConstant){
+app.controller('SettingsPerspectivesCtrl', ['$scope', '$log', '$timeout', '$mdDialog', '$state', 'trix', 'FileUploader', 'TRIX', 'cfpLoadingBar', '$mdDialog', '$mdToast', '$filter', '$translate', '$mdConstant', '$mdSidenav',
+	function($scope ,  $log ,  $timeout ,  $mdDialog ,  $state, trix, FileUploader, TRIX, cfpLoadingBar, $mdDialog, $mdToast, $filter, $translate, $mdConstant, $mdSidenav){
 
+$scope.togglePerspectives = buildToggler('perspective-list');
+
+	function buildToggler(navID) {
+    return function() {
+      $mdSidenav(navID)
+        .toggle()
+    }
+  }
 
 
 var settingsPerspectivesCtrl = $scope;
