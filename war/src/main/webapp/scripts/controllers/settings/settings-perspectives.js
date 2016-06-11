0da657f43da5d9668200f2ea9a4514cd6e482145
Misael Neto <misawsneto@gmail.com>
2016-06-11 12:22:45 -0300

    perspective upgrades

--- war/src/main/webapp/scripts/controllers/settings/settings-perspectives.js
+++ war/src/main/webapp/scripts/controllers/settings/settings-perspectives.js
@@ -1,18 +1,48 @@
-app.controller('SettingsPerspectivesCtrl', ['$scope', '$log', '$timeout', '$mdDialog', '$state', 'trix', 'FileUploader', 'TRIX', 'cfpLoadingBar', '$mdDialog', '$mdToast', '$filter', '$translate', '$mdConstant', '$mdSidenav',
-	function($scope ,  $log ,  $timeout ,  $mdDialog ,  $state, trix, FileUploader, TRIX, cfpLoadingBar, $mdDialog, $mdToast, $filter, $translate, $mdConstant, $mdSidenav){
+app.controller('SettingsPerspectivesCtrl', ['$scope', '$log', '$timeout', '$mdDialog', '$state', 'trix', 'FileUploader', 'TRIX', 'cfpLoadingBar', '$mdDialog', '$mdToast', '$filter', '$translate', '$mdConstant', '$mdSidenav', 'station',
+	function($scope ,  $log ,  $timeout ,  $mdDialog ,  $state, trix, FileUploader, TRIX, cfpLoadingBar, $mdDialog, $mdToast, $filter, $translate, $mdConstant, $mdSidenav, station ){
 
-$scope.togglePerspectives = buildToggler('perspective-list');
+	$scope.togglePerspectives = buildToggler('perspective-list');
 
-	function buildToggler(navID) {
-    return function() {
-      $mdSidenav(navID)
-        .toggle()
-    }
+	$scope.thisStation = station;
+
+		function buildToggler(navID) {
+	    return function() {
+	      $mdSidenav(navID)
+	        .toggle()
+	    }
+  }
+
+  var addSnippet = function(postObj){
+    if(postObj.body)
+      postObj.snippet = postObj.body.simpleSnippet();
+  }
+
+  $scope.getSnippet = function(body){
+    if(body)
+      return body.simpleSnippet();
   }
 
 
-var settingsPerspectivesCtrl = $scope;
+  trix.getStationStationPerspectives($scope.thisStation.id).success(function(response){
+    $scope.stationPerspectives = response.stationPerspectives;
+      $scope.stationPerspectives.forEach(function(perspective, index){
+        if(perspective.id == $scope.thisStation.defaultPerspectiveId){
+          perspective.selected = true;
+          $scope.currentPerspective = perspective;
+        }
+      getPerspectiveView(perspective)
+    });
+  })
+
+var getPerspectiveView = function(perspective){
+  trix.findPerspectiveView(perspective.id, null, null, 0, 50).success(function(termPerspective){
+    perspective.termPerspectiveView = termPerspective;
+  })
+}
+
+
+settingsPerspectivesCtrl = $scope;
 
 }]);
 
-settingsPerspectivesCtrl = null;
+var settingsPerspectivesCtrl = null;
