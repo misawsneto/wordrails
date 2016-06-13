33fff3bd43a16301a0a89702df1d08d4da711309
Misael Neto <misawsneto@gmail.com>
2016-06-13 19:44:47 -0300

    paginate home

--- war/src/main/webapp/scripts/controllers/app/page.js
+++ war/src/main/webapp/scripts/controllers/app/page.js
@@ -35,6 +35,35 @@ app.controller('PageCtrl', ['$scope', '$rootScope', '$log', '$timeout', '$mdDial
 	      }
 	    }
 
+	    $scope.page = 1;
+
+	    if($scope.app.termPerspectiveView.homeRow && $scope.app.termPerspectiveView.homeRow.cells){
+	    	$scope.app.termPerspectiveView.homeRow.allLoaded = false;
+	    	var length = $scope.app.termPerspectiveView.homeRow.cells.length >= 10 ? 10 : $scope.app.termPerspectiveView.homeRow.cells.length;
+	    	$scope.app.termPerspectiveView.homeRow.cells = $scope.app.termPerspectiveView.homeRow.cells.slice(0,length);
+	    }
+
+	    $scope.loadingPerspective = false;
+	    $scope.app.perspectivePaginate = function(){
+	    	$scope.app.termPerspectiveView.homeRow.page = $scope.page
+	    	if(!$scope.loadingPerspective && $scope.app.termPerspectiveView.homeRow && $scope.app.termPerspectiveView.homeRow.cells && !$scope.app.termPerspectiveView.homeRow.allLoaded){
+		    	$scope.loadingPerspective = true;
+		    	trix.getRowView($scope.app.currentStation.defaultPerspectiveId, $scope.app.termPerspectiveView.id, null, $scope.page, 20)
+		    	.success(function(response){
+		            if(response.cells && response.cells.length > 0){
+		                response.cells.forEach(function(cell, index){
+		                   $scope.app.termPerspectiveView.homeRow.cells.push(cell)
+		               	});
+		                $scope.page = $scope.app.termPerspectiveView.homeRow.page = $scope.app.termPerspectiveView.homeRow.page + 1
+		                $scope.reloadMasonry();
+		                $scope.loadingPerspective = false;
+		            }else{
+		                $scope.app.termPerspectiveView.homeRow.allLoaded = true;
+		            }
+
+		    	})
+    		}
+	    }
 
 }]);
 
