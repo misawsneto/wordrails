ebac2c8dee2e59b831f29f09b62b9ff5d0ebbbe9
jonasagx <jonas.agx@gmail.com>
2016-06-18 16:52:12 -0300

    Validation base methods

--- jar/src/main/java/co/xarx/trix/persistence/PersonValidationRepository.java
+++ jar/src/main/java/co/xarx/trix/persistence/PersonValidationRepository.java
@@ -1,4 +1,15 @@
 package co.xarx.trix.persistence;
 
-public interface PersonValidationRepository {
+import co.xarx.trix.domain.Person;
+import co.xarx.trix.domain.PersonValidation;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+public interface PersonValidationRepository extends DatabaseRepository<PersonValidation, Integer> {
+
+	@RestResource(path = "findBValidationHash")
+	PersonValidation findByHash(@Param("hash") String hash);
+
+	@RestResource(exported = false)
+	PersonValidation findByPerson(@Param("person") Person person);
 }
