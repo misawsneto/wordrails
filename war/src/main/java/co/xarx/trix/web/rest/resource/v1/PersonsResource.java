d14944ee2930f0b590340981456e1751aed3fd9e
jonasagx <jonas.agx@gmail.com>
2016-06-18 17:05:26 -0300

    Make resource null-safe and new method to manage station permissions

--- war/src/main/java/co/xarx/trix/web/rest/resource/v1/PersonsResource.java
+++ war/src/main/java/co/xarx/trix/web/rest/resource/v1/PersonsResource.java
@@ -29,6 +29,7 @@ import co.xarx.trix.web.rest.AbstractResource;
 import co.xarx.trix.web.rest.api.v1.PersonsApi;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mysema.commons.lang.Assert;
 import lombok.NoArgsConstructor;
 import org.apache.http.util.Asserts;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -325,7 +326,7 @@ public class PersonsResource extends AbstractResource implements PersonsApi {
 	@Override
 	public Response addPerson(PersonCreateDto dto) throws ConflictException, BadRequestException, IOException {
 		try {
-			Person person = personService.createPerson(dto);
+			Person person = personService.addPerson(dto);
 			return Response.status(Status.CREATED).entity(mapper.writeValueAsString(person)).build();
 		} catch (PersonAlreadyExistsException e) {
 			e.printStackTrace();
@@ -340,12 +341,24 @@ public class PersonsResource extends AbstractResource implements PersonsApi {
 			return Response.status(Status.CREATED).entity(mapper.writeValueAsString(person)).build();
 		} catch (PersonAlreadyExistsException e) {
 			e.printStackTrace();
-			throw new BadRequestException(e.getMessage());
+			return Response.status(Status.CONFLICT).build();
+		}
+	}
+
+	@Override
+	public Response signUpFromInvite(PersonCreateDto dto, String inviteHash) throws ConflictException, BadRequestException, IOException {
+		try {
+			Person person = personService.createFromInvite(dto, inviteHash);
+			return Response.status(Status.CREATED).entity(mapper.writeValueAsString(person)).build();
+		} catch (PersonAlreadyExistsException e) {
+			e.printStackTrace();
+			return Response.status(Status.CONFLICT).build();
 		}
 	}
 
 	@Override
 	public Response validatePersonEmail(String hash){
+		Assert.hasText(hash, "A valid hash mush be sent");
 		if(personService.validateEmail(hash) == null){
 			return Response.status(Status.NOT_FOUND).build();
 		}
@@ -353,12 +366,15 @@ public class PersonsResource extends AbstractResource implements PersonsApi {
 	}
 
 	@Override
-	public Response invitePerson(PersonsApi.PersonInvitationDto dto) throws ConflictException, BadRequestException,
-			IOException {
-		personService.invite(dto);
+	public Response invitePerson(PersonsApi.PersonInvitationDto dto) throws ConflictException, IOException {
+		try{
+			personService.invite(dto);
+		} catch (BadRequestException e){
+			e.printStackTrace();
+			return Response.status(Status.BAD_REQUEST).build();
+		}
 		return Response.status(Status.CREATED).build();
 	}
-
 
 	@Override
 	public ContentResponse<Integer> countPersonsByNetwork(@QueryParam("q") String q) {
@@ -400,22 +416,24 @@ public class PersonsResource extends AbstractResource implements PersonsApi {
 	public Response disablePerson(Integer personId) {
 		Person self = authProvider.getLoggedPerson();
 		Person person = personRepository.findOne(personId);
-		if (!self.id.equals(person.id)) {
+		if (person != null && !self.id.equals(person.id)) {
 			person.user.enabled = false;
 			personRepository.save(person);
+			return Response.status(Status.CREATED).build();
 		}
-		return Response.status(Status.CREATED).build();
+		return Response.status(Status.NOT_FOUND).build();
 	}
 
 	@Override
 	public Response enablePerson(Integer personId) {
 		Person self = authProvider.getLoggedPerson();
 		Person person = personRepository.findOne(personId);
-		if (!self.id.equals(person.id)) {
+		if (person != null && !self.id.equals(person.id)) {
 			person.user.enabled = true;
 			personRepository.save(person);
+			return Response.status(Status.CREATED).build();
 		}
-		return Response.status(Status.CREATED).build();
+		return Response.status(Status.NOT_FOUND).build();
 	}
 
 	@Override
