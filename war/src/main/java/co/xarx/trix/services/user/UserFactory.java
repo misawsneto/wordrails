8bbd8fedd270aee3f02b5e15a695d457c8462cbe
jonasagx <jonas.agx@gmail.com>
2016-06-18 16:26:25 -0300

    Added validation feature to uesr creation

--- war/src/main/java/co/xarx/trix/services/user/UserFactory.java
+++ war/src/main/java/co/xarx/trix/services/user/UserFactory.java
@@ -1,9 +1,12 @@
 package co.xarx.trix.services.user;
 
 import co.xarx.trix.annotation.IntegrationTestBean;
+import co.xarx.trix.config.multitenancy.TenantContextHolder;
+import co.xarx.trix.domain.Network;
 import co.xarx.trix.domain.User;
 import co.xarx.trix.domain.UserGrantedAuthority;
 import co.xarx.trix.domain.social.SocialUser;
+import co.xarx.trix.persistence.NetworkRepository;
 import co.xarx.trix.persistence.UserGrantedAuthorityRepository;
 import co.xarx.trix.persistence.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,14 +17,16 @@ import org.springframework.stereotype.Service;
 public class UserFactory {
 
 	private UserRepository userRepository;
+	private NetworkRepository networkRepository;
 	private UserConnectionFactory userConnectionFactory;
 	private UserGrantedAuthorityRepository grantedAuthorityRepository;
 
 	@Autowired
-	public UserFactory(UserRepository userRepository, UserConnectionFactory userConnectionFactory, UserGrantedAuthorityRepository grantedAuthorityRepository) {
+	public UserFactory(UserRepository userRepository, UserConnectionFactory userConnectionFactory, UserGrantedAuthorityRepository grantedAuthorityRepository, NetworkRepository networkRepository) {
 		this.userRepository = userRepository;
 		this.userConnectionFactory = userConnectionFactory;
 		this.grantedAuthorityRepository = grantedAuthorityRepository;
+		this.networkRepository = networkRepository;
 	}
 
 	public User create(String username, String password) throws UserAlreadyExistsException {
@@ -29,8 +34,11 @@ public class UserFactory {
 			throw new UserAlreadyExistsException();
 		}
 
+		String tenantId = TenantContextHolder.getCurrentTenantId();
+		Network network = networkRepository.findByTenantId(tenantId);
+
 		User user = new User();
-		user.enabled = true;
+		user.enabled = !network.isEmailSignUpValidationEnabled();
 		user.username = username;
 		user.password = password;
 
