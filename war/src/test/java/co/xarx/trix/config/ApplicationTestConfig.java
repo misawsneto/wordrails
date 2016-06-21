caf40f8c9833824aa81377e834dc1c22ff475d0f
Misael Neto <misawsneto@gmail.com>
2016-06-21 10:03:19 -0300

    many fixes

--- war/src/test/java/co/xarx/trix/config/ApplicationTestConfig.java
+++ war/src/test/java/co/xarx/trix/config/ApplicationTestConfig.java
@@ -7,12 +7,18 @@ import co.xarx.trix.config.multitenancy.TenantProvider;
 import co.xarx.trix.elasticsearch.mapper.*;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.modelmapper.ModelMapper;
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.lang.reflect.Method;
+import java.util.concurrent.Executor;
 
 import static org.springframework.context.annotation.FilterType.ANNOTATION;
 
@@ -25,7 +31,7 @@ import static org.springframework.context.annotation.FilterType.ANNOTATION;
 		includeFilters = {
 				@ComponentScan.Filter(type = ANNOTATION, value = {IntegrationTestBean.class})
 		})
-public class ApplicationTestConfig {
+public class ApplicationTestConfig implements AsyncConfigurer{
 
 	@Bean
 	public ObjectMapper simpleMapper() {
@@ -54,6 +60,28 @@ public class ApplicationTestConfig {
 		modelMapper.addMappings(new StationViewMap());
 		modelMapper.addMappings(new PostViewMap());
 		return modelMapper;
+	}
+
+	@Override
+	public Executor getAsyncExecutor() {
+		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+		executor.setCorePoolSize(5);
+		executor.setMaxPoolSize(20);
+		executor.setQueueCapacity(100);
+		executor.setThreadNamePrefix("Async-");
+		executor.initialize();
+		return executor;
+	}
+
+	@Override
+	public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+		return new AsyncUncaughtExceptionHandler() {
+			@Override
+			public void handleUncaughtException(Throwable throwable, Method method, Object... objects) {
+				throwable.printStackTrace();
+				TenantContextHolder.setCurrentTenantId(null);
+			}
+		};
 	}
 
 	@Bean
