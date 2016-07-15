ba7dda68864b62e2d173e26a8b3863c7dfe303a1
Misael Neto <misawsneto@gmail.com>
2016-07-15 16:03:38 -0300

    removing media library

--- jar/src/main/java/co/xarx/trix/persistence/PersonValidationRepository.java
+++ jar/src/main/java/co/xarx/trix/persistence/PersonValidationRepository.java
@@ -7,8 +7,8 @@ import org.springframework.data.rest.core.annotation.RestResource;
 
 public interface PersonValidationRepository extends DatabaseRepository<PersonValidation, Integer> {
 
-	@RestResource(path = "findBValidationHash")
-	PersonValidation findByHash(@Param("hash") String hash);
+	@RestResource(path = "findByValidationHash")
+	PersonValidation findByValidationHash(@Param("hash") String hash);
 
 	@RestResource(exported = false)
 	PersonValidation findByPerson(@Param("person") Person person);
