dfd67de2fcb6f43beccaf5af3d4cd4523d708b28
Jonas Xavier <jonas.agx@gmail.com>
2016-07-20 18:31:48 -0300

    Doing better stats with internal search service

--- war/src/main/java/co/xarx/trix/services/comment/ESCommentService.java
+++ war/src/main/java/co/xarx/trix/services/comment/ESCommentService.java
@@ -47,6 +47,7 @@ public class ESCommentService extends AbstractElasticSearchService {
 		applyShouldFilter(f, params.getAuthors(), "authorId");
 		applyShouldFilter(f, params.getStations(), "stationId");
 		applyShouldFilter(f, params.getPosts(), "postId");
+		applyShouldFilter(f, params.getTenants(), "tenantId");
 
 
 		SearchQuery query = getSearchQuery(params.getQuery(), sorts, q, f);
