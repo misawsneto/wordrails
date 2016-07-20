63e655b21d82c734a6642d3beb80ec6e732b80d7
Jonas Xavier <jonas.agx@gmail.com>
2016-07-20 18:34:22 -0300

    Refacotoring stats data classes

--- api/src/main/java/co/xarx/trix/api/v2/MobileStats.java
+++ api/src/main/java/co/xarx/trix/api/v2/MobileStats.java
@@ -1,5 +1,6 @@
 package co.xarx.trix.api.v2;
 
+import co.xarx.trix.util.Constants;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
@@ -12,12 +13,13 @@ import java.io.Serializable;
 @Setter
 @NoArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
-@JsonIgnoreProperties(ignoreUnknown = true)
-public class StoreStatsData implements Serializable{
+@JsonIgnoreProperties(ignoreUnknown = true, value = {"type"})
+public class MobileStats implements Serializable {
 //	public float averageRaiting;
 //	public Integer downloads;
 	public Integer currentInstallations;
 	public Integer weeklyActiveUsers;
 	public Integer monthlyActiveUsers;
 	public Integer monthlyDownloads;
+	public Constants.MobilePlatform type;
 }
