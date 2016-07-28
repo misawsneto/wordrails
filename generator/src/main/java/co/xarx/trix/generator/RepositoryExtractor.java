c55721f3deaa68f7bdea155cce41d673a071f5bd
Misael Neto <misawsneto@gmail.com>
2016-07-28 05:33:43 -0300

    parameter order fix attempt

--- generator/src/main/java/co/xarx/trix/generator/RepositoryExtractor.java
+++ generator/src/main/java/co/xarx/trix/generator/RepositoryExtractor.java
@@ -140,6 +140,10 @@ public class RepositoryExtractor {
 
 		annotatedTypes.putAll(pageableTypes); //ensures that pageable is always last
 
+		if("findPersons".equals(method.getName())){
+			String.valueOf(true);
+		}
+
 		query.parameters = getQueryParameters(annotatedTypes);
 
 		return query;
@@ -150,7 +154,17 @@ public class RepositoryExtractor {
 
 		for (Type type : annotatedTypes.keySet()) {
 			Annotation[] annotationz = annotatedTypes.get(type);
-			if (Pageable.class.equals(type)) {
+			if (!Pageable.class.equals(type)) {
+				FieldDescription parameter = new FieldDescription();
+				parameter.type = getType(type);
+				for (Annotation annotation : annotationz) {
+					if (annotation instanceof Param) {
+						parameter.name = ((Param) annotation).value();
+						break;
+					}
+				}
+				parameters.add(parameter);
+			} else {
 				FieldDescription page = new FieldDescription();
 				page.name = "page";
 				page.type = getType(Integer.class);
@@ -165,17 +179,6 @@ public class RepositoryExtractor {
 				sort.name = "sort";
 				sort.type = "List<String>";
 				parameters.add(sort);
-
-			} else {
-				FieldDescription parameter = new FieldDescription();
-				parameter.type = getType(type);
-				for (Annotation annotation : annotationz) {
-					if(annotation instanceof Param) {
-						parameter.name = ((Param) annotation).value();
-						break;
-					}
-				}
-				parameters.add(parameter);
 			}
 		}
 
