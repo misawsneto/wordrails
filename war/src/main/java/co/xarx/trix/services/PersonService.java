d695dd7762be6230ce767bad4cbf1cb698d31ff2
Jonas Xavier <jonas.agx@gmail.com>
2016-08-08 18:27:12 -0300

    Fix tenantId null and better exception handling

--- war/src/main/java/co/xarx/trix/services/PersonService.java
+++ war/src/main/java/co/xarx/trix/services/PersonService.java
@@ -24,6 +24,7 @@ import org.joda.time.format.ISODateTimeFormat;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
 
 import java.io.IOException;
@@ -44,7 +45,6 @@ public class PersonService {
     private PersonFactory personFactory;
     private NetworkService networkService;
     private PersonRepository personRepository;
-    private NetworkRepository networkRepository;
     private StationRepository stationRepository;
     private InvitationRepository invitationRepository;
     private StationPermissionService stationPermissionService;
@@ -60,24 +60,18 @@ public class PersonService {
 		this.nginxAccessIndex = nginxAccessIndex;
 		this.personRepository = personRepository;
 		this.stationRepository = stationRepository;
-		this.networkRepository = networkRepository;
 		this.invitationRepository = invitationRepository;
 		this.stationPermissionService = stationPermissionService;
 		this.personValidationRepository = personValidationRepository;
         this.networkService = networkService;
     }
 
+    @Transactional
 	public Person createFromInvite(PersonsApi.PersonCreateDto dto, String inviteHash) throws PersonAlreadyExistsException, UserAlreadyExistsException {
 		Assert.hasText(inviteHash);
 
 		User newUser = userFactory.create(dto.username, dto.password);
-		Person newPerson = null;
-		try {
-			newPerson = personFactory.create(dto.name, dto.email, newUser);
-		} catch (Exception e) {
-			userFactory.delete(dto.username);
-			throw e;
-		}
+		Person newPerson = personFactory.create(dto.name, dto.email, newUser);
 
 		Invitation invitation = invitationRepository.findByHash(inviteHash);
 
@@ -94,20 +88,15 @@ public class PersonService {
 		return newPerson;
 	}
 
+	@Transactional
 	public Person createPerson(PersonsApi.PersonCreateDto dto) throws PersonAlreadyExistsException, UserAlreadyExistsException, IOException {
 		if (dto.password == null || dto.password.isEmpty())
 			dto.password = StringUtil.generateRandomString(6, "aA#");
 
 		User newUser = userFactory.create(dto.username, dto.password);
-		Person newPerson;
-		try{
-			newPerson = personFactory.create(dto.name, dto.email, newUser);
-		}catch (Exception e){
-			userFactory.delete(dto.username);
-			throw e;
-		}
+		Person newPerson = personFactory.create(dto.name, dto.email, newUser);
 
-		Network network = networkRepository.findByTenantId(TenantContextHolder.getCurrentTenantId());
+		Network network = networkService.getNetwork();
 
 		PersonValidation validation = new PersonValidation();
 		validation.setPerson(newPerson);
@@ -121,21 +110,15 @@ public class PersonService {
 		return newPerson;
 	}
 
+	@Transactional
 	public Person addPerson(PersonsApi.PersonCreateDto dto) throws PersonAlreadyExistsException, UserAlreadyExistsException {
 		if(dto.password == null || dto.password.isEmpty())
 			dto.password = StringUtil.generateRandomString(6, "aA#");
 
 		User newUser = userFactory.create(dto.username, dto.password);
-		Person newPerson;
-		try {
-			newPerson = personFactory.create(dto.name, dto.email, newUser);
-		} catch (Exception e) {
-			userFactory.delete(dto.username);
-			throw e;
-		}
+		Person newPerson = personFactory.create(dto.name, dto.email, newUser);
 
-		Network network = networkRepository.findByTenantId(TenantContextHolder.getCurrentTenantId());
-
+		Network network = networkService.getNetwork();
 		try {
 			emailService.sendCredentials(network, authService.getLoggedPerson(), newPerson);
 		} catch (Exception e) {
@@ -151,8 +134,7 @@ public class PersonService {
 			return null;
 		}
 
-		String tenantId = TenantContextHolder.getCurrentTenantId();
-		Network network = networkRepository.findByTenantId(tenantId);
+		Network network = networkService.getNetwork();
 
 		Person person = validation.getPerson();
 		validation.validated = true;
@@ -164,6 +146,16 @@ public class PersonService {
 
 		personValidationRepository.save(validation);
 		return person;
+	}
+
+	private void cleanInvitationDto(PersonsApi.PersonInvitationDto dto){
+		removeRepeatedPerson(dto.emails);
+		removeInvalidStations(dto.stationIds);
+
+		dto.emailTemplate = dto.emailTemplate.replaceAll("\\&\\#123;\\&\\#123;", "{{");
+		dto.emailTemplate = dto.emailTemplate.replaceAll("\\&\\#125;\\&\\#125;", "}}");
+		dto.emailTemplate = dto.emailTemplate.replaceAll("\\%7B\\%7B", "{{");
+		dto.emailTemplate = dto.emailTemplate.replaceAll("\\%7D\\%7D", "}}");
 	}
 
 	private void removeInvalidStations(List stations){
@@ -195,15 +187,8 @@ public class PersonService {
 			throw new BadRequestException("Invalid emails or template");
 		}
 
-		removeRepeatedPerson(dto.emails);
-		removeInvalidStations(dto.stationIds);
-
-		Network network = networkRepository.findByTenantId(TenantContextHolder.getCurrentTenantId());
-
-		dto.emailTemplate = dto.emailTemplate.replaceAll("\\&\\#123;\\&\\#123;", "{{");
-		dto.emailTemplate = dto.emailTemplate.replaceAll("\\&\\#125;\\&\\#125;", "}}");
-		dto.emailTemplate = dto.emailTemplate.replaceAll("\\%7B\\%7B", "{{");
-		dto.emailTemplate = dto.emailTemplate.replaceAll("\\%7D\\%7D", "}}");
+		cleanInvitationDto(dto);
+		Network network = networkService.getNetwork();
 
 		for(String email: dto.emails){
 			Invitation invitation = new Invitation(network.getRealDomain());
@@ -223,14 +208,12 @@ public class PersonService {
 		Person person = personRepository.findByUsername(username);
 		Assert.notNull(person, "Person not found: personId " + username);
 
-		List<Map<String, Object>> timeline = new ArrayList<>();
-
 		SearchRequestBuilder search = client.prepareSearch(nginxAccessIndex);
 		search.setQuery(boolQuery().must(termQuery("username", person.username)));
-
 		SearchResponse searchResponse = search.execute().actionGet();
-
 		DateTimeFormatter fmt = ISODateTimeFormat.dateTime();
+
+		List<Map<String, Object>> timeline = new ArrayList<>();
 
 		SearchHit[] hits = searchResponse.getHits().getHits();
 		for (SearchHit hit : hits) {
