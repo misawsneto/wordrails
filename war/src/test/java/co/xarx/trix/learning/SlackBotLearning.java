722dcfd2989fefff3e80d36165c26b8693237d82
Jonas Xavier <jonas.agx@gmail.com>
2016-09-01 16:14:55 -0300

    Learning to use the Slack API

--- war/src/test/java/co/xarx/trix/learning/SlackBotLearning.java
+++ war/src/test/java/co/xarx/trix/learning/SlackBotLearning.java
@@ -1,7 +1,20 @@
 package co.xarx.trix.learning;
 
-/**
- * Created by guto on 9/1/16.
- */
+import com.ullink.slack.simpleslackapi.SlackChannel;
+import com.ullink.slack.simpleslackapi.SlackSession;
+import com.ullink.slack.simpleslackapi.impl.SlackSessionFactory;
+
+import java.io.IOException;
+
 public class SlackBotLearning {
+
+    public static void main(String[] args) throws IOException {
+        SlackSession session = SlackSessionFactory.createWebSocketSlackSession("a-string-token");
+        session.connect();
+        System.out.println(session.getTeam().getName());
+
+        SlackChannel channel = session.findChannelByName("trix-log");
+        session.sendMessage(channel, "Hey there");
+        session.disconnect();
+    }
 }
