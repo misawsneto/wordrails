c85fb1c622214fea94a6c95b94915359e9c8b6dc
Misael Neto <misawsneto@gmail.com>
2016-09-05 08:10:48 -0300

    fix rollback error

--- war/src/main/java/co/xarx/trix/services/NetworkService.java
+++ war/src/main/java/co/xarx/trix/services/NetworkService.java
@@ -115,8 +115,8 @@ public class NetworkService {
 
         if(network.getValidationMessage() == null || network.getValidationMessage().isEmpty()){
             String message = FileUtil.loadFileFromResource("default_validation_text.txt");
-        	network.setValidationMessage(message);
-			networkRepository.save(network);
+//        	network.setValidationMessage(message);
+//			networkRepository.save(network);
 		}
 
 		return htmlTemplate.replaceAll("\\{\\{validationMessage}}", network.getValidationMessage());
