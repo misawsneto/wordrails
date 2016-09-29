aa745bf3c05f063f594a7736e70cae16e1ae5c1c
Jonas Xavier <jonas.agx@gmail.com>
2016-09-29 16:41:43 -0300

    Term/Category seen

--- war/src/main/java/co/xarx/trix/web/rest/resource/v2/V2TermsResource.java
+++ war/src/main/java/co/xarx/trix/web/rest/resource/v2/V2TermsResource.java
@@ -1,7 +1,38 @@
 package co.xarx.trix.web.rest.resource.v2;
 
-/**
- * Created by guto on 9/28/16.
- */
-public class V2TermsResource {
+import co.xarx.trix.domain.Term;
+import co.xarx.trix.persistence.TermRepository;
+import co.xarx.trix.services.analytics.RequestWrapper;
+import co.xarx.trix.services.analytics.StatEventsService;
+import co.xarx.trix.web.rest.AbstractResource;
+import co.xarx.trix.web.rest.api.v2.V2TermsApis;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
+
+import javax.ws.rs.core.Response;
+import java.util.Date;
+
+@Component
+public class V2TermsResource extends AbstractResource implements V2TermsApis {
+
+    private TermRepository termRepository;
+    private StatEventsService statEventsService;
+
+    @Autowired
+    public V2TermsResource(TermRepository termRepository, StatEventsService statEventsService){
+        this.termRepository = termRepository;
+        this.statEventsService = statEventsService;
+    }
+
+    @Override
+    public Response setTermSeen(Integer id, Integer timeReading, Long timestamp) {
+        Term term = termRepository.findOne(id);
+        Assert.notNull(term, "Term not found");
+        RequestWrapper rw = new RequestWrapper(request);
+
+        Date date = timestamp != null ? new Date(timestamp) : new Date();
+        statEventsService.newTermView(term, rw, timeReading, date);
+        return Response.ok().build();
+    }
 }
