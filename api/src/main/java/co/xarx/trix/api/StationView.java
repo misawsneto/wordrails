ce486be509de139ba8c68b94ea2dfb39005d42b3
Cassio Carvalho <lccassiol@gmail.com>
2017-10-16 21:57:00 -0300

    Implement method login with parameters updatelocation

--- api/src/main/java/co/xarx/trix/api/StationView.java
+++ api/src/main/java/co/xarx/trix/api/StationView.java
@@ -1,6 +1,7 @@
 package co.xarx.trix.api;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -17,6 +18,7 @@ import java.util.Map;
 import java.util.Set;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class StationView implements Serializable {
 
 	private static final long serialVersionUID = 2504331733945628049L;
--- api/src/main/java/co/xarx/trix/api/TermView.java
+++ api/src/main/java/co/xarx/trix/api/TermView.java
@@ -1,8 +1,15 @@
 package co.xarx.trix.api;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
 import java.io.Serializable;
 import java.util.List;
 
+@Data
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class TermView implements Serializable {
 	private static final long serialVersionUID = 5504321793935627049L;
 
