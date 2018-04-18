5f558b9b7eb89d3dd0e5611a8cc3fe0eb61a2c2f
Cassio Carvalho <lccassiol@gmail.com>
2018-04-18 08:47:20 -0300

    Create object CouponView

---
+++ api/src/main/java/co/xarx/trix/api/CouponView.java
@@ -0,0 +1,53 @@
+package co.xarx.trix.api;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class CouponView {
+    public String eSId;
+    Integer postId;
+    Integer stationId;
+    Long siteId;
+
+    String title;
+    Date date;
+
+    Date useDate;
+    String user;
+
+    Integer classifiedId;
+
+    String smallImageUrl;
+    String mediumImageUrl;
+    String largeImageUrl;
+
+    Integer authorId;
+    String authorName;
+    String authorEmail;
+
+    String discountType;
+    int amount;
+    boolean freeShipping;
+    Date expiryDate;
+    int minimumSpend;
+    int maximumSpend;
+    boolean individualUse;
+    boolean excludeSalesItem;
+    int usageLimit;
+    int usageCount;
+
+    //private Integer product;  Not using yet
+    //private List<Integer> excludeProducts; Not using yet
+    List<String> productCategories;
+    List<Integer> excludeCategories;
+
+    String emailRestriction;
+    Integer usageLimitToXItem;
+    int usageLimitPerUser;
+
+    LocationData location;
+}
